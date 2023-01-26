062b562f13db7b71bd82cb40b1063dc1784a9575
dev-library <devrary@gmail.com>
2023-01-27 00:07:02 +0900

    데브러리 : 로깅 준비

--- src/test/java/com/example/shorturl/repository/ShortUrlRepositoryTests.java
+++ src/test/java/com/example/shorturl/repository/ShortUrlRepositoryTests.java
@@ -1,12 +1,14 @@
 package com.example.shorturl.repository;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ShortUrlRepositoryTests {
-
+    private final Logger log = LoggerFactory.getLogger(getClass());
     @Autowired
     IShortUrlRepository shortUrlRepository;
 
--- src/test/java/com/example/shorturl/service/ShortUrlServiceTests.java
+++ src/test/java/com/example/shorturl/service/ShortUrlServiceTests.java
@@ -1,12 +1,14 @@
 package com.example.shorturl.service;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ShortUrlServiceTests {
-
+    private final Logger log = LoggerFactory.getLogger(getClass())
     @Autowired
     IShortUrlService shortUrlService;
 
