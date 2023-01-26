1d4be1db87409289fb064c36f2a3a5b492a3ea31
dev-library <devrary@gmail.com>
2023-01-26 22:37:15 +0900

    데브러리 : 테스트코드 틀 만들기

---
+++ src/test/java/com/example/shorturl/service/ShortUrlServiceTests.java
@@ -0,0 +1,18 @@
+package com.example.shorturl.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ShortUrlServiceTests {
+
+    @Autowired
+    IShortUrlService shortUrlService;
+
+    @Test
+    public void test(){
+
+    }
+
+}
